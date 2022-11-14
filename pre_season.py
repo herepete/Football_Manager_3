8f5e9e394eef2da321cbe4c2b9c0dfb847d10c6a
herepete <herepete@hotmail.com>
2022-11-14 16:00:22 +0000

    working on pre_season -code still being written and tested

--- pre_season.py
+++ pre_season.py
@@ -12,217 +12,109 @@ else:
     print("verbosity not turned on")
 
 
-test_squad=[[['GK'], 'Junior', 'Mander', 25, 85, 7, 2, '**', 'TP', 0, '', 1484615], [['GK'], 'Bob', 'Shearer', 21, 80, 2, 3, '***', 'None', 0, '', 7791753], [['GK'], 'James', 'Charles', 24, 76, 4, 1, '****', 'None', 0, '', 4444531], [['CB'], 'Simon', 'Barrett', 26, 72, 6, 1, '**', 'None', 0, '', 6829857], [['LB', 'RB'], 'James', 'Zhang', 27, 81, 6, 3, '**', 'LB', 0, '', 4446501], [['CB', 'LB', 'RB'], 'Zak', 'Manning', 36, 77, 1, 4, '**', 'TP', 0, '', 4604175], [['LB'], 'Zak', 'Teng', 25, 78, 4, 4, '****', 'None', 0, '', 2624641], [['CB'], 'Bob', 'Manning', 35, 93, 12, 1, '***', 'L', 0, '', 3289916], [['LB'], 'Barry', 'Ali', 20, 91, 9, 3, '**', 'None', 0, '', 9289061], [['LB'], 'Symon', 'Teng', 27, 90, 8, 3, '**', 'None', 0, '', 8583758], [['CB'], 'Master', 'Smith', 28, 95, 15, 3, '***', 'LB', 0, '', 4158431], [['AM'], 'Me', 'Racker', 32, 89, 8, 2, '***', 'None', 0, '', 2049741], [['RM'], 'Paul', 'Roberts', 26, 82, 6, 4, '**', 'TP', 0, '', 5201992], [['CM', 'DM'], 'Silver', 'Curtis', 30, 71, 4, 2, '*', 'None', 0, '', 1916288], [['DM'], 'Tony', 'Bishop', 35, 74, 4, 3, '**', 'LB', 0, '', 699461], [['CM'], 'Paul', 'Garrett', 34, 87, 7, 1, '***', 'LB', 0, '', 401208], [['CM'], 'Nathan', 'Garrett', 18, 93, 12, 4, '****', 'None', 0, '', 8818879], [['LM'], 'Dylon', 'Tucker', 19, 83, 6, 1, '**', '5SR', 0, '', 7549736], [['DM'], 'Dylon', 'Zhang', 21, 90, 8, 1, '***', '5SR', 0, '', 2713236], [['RW'], 'Barry', 'Omar', 22, 71, 1, 3, '**', 'None', 0, '', 8199760], [['ST'], 'See', 'Hutch', 22, 86, 7, 3, '****', 'None', 0, '', 3850846], [['LW', 'RW'], 'Symon', 'Mayfield', 29, 76, 4, 2, '**', 'None', 0, '', 5050682], [['RW'], 'Li', 'Shearer', 20, 77, 1, 4, '**', 'None', 0, '', 3733897], [['RW'], 'Blesing', 'Garrett', 28, 90, 8, 2, '***', 'TP', 0, '', 5246984]]
 
-#                gk,lb,rb,cb,lm,dm,cm,rm,am,lw,st,rw]
 formation_4_4_2={'GK':1,"LB":1,"RB":1,"CB":2,"DM":0,"CM":2,"LM":1,"RM":1,"AM":0,"LW":0,"ST":2,"RW":0}
 formation_4_4_2_just_needed={k:v for (k,v) in formation_4_4_2.items() if v > 0}
 formation_4_4_2_list_keys=list(formation_4_4_2_just_needed.keys())
 
+test_squad=[[['GK'], 'Rambo', 'Kane', 20, 16, 9, 14, 20, 14, 14, 75, 2, 1, 0, 'Avg', '****', 0, 'DP', 821920], [['GK'], 'Li', 'Mayfield', 19, 12, 5, 19, 12, 18, 17, 61, 5, 4, 1, 'Leader', '*****', 0, 'DP', 607207], [['GK'], 'Paul', 'Roberts', 27, 13, 13, 7, 18, 19, 13, 61, 2, 4, 0, 'Avg', '*', 0, 'DP', 2791431], ['LB', 'Ross', 'Kane', 25, 3, 19, 19, 9, 19, 8, 78, 4, 2, 1, 'Fighter', '**', 0, 'DP', 5449644], ['LB', 'Gibby', 'Barrett', 29, 1, 15, 13, 6, 16, 20, 73, 4, 4, 0, 'Laid B', '**', 0, 'DP', 2472907], ['RB', 'Peter', 'Del-Piero', 32, 3, 13, 17, 14, 10, 9, 60, 6, 4, 1, 'Team P', '**', 0, 'DP', 7409013], ['RB', 'TJ', 'Mohammed', 19, 1, 9, 16, 12, 10, 6, 48, 1, 4, 1, 'Team P', '*****', 0, 'DP', 5781168], ['RB', 'See', 'Mander', 35, 1, 10, 7, 17, 15, 8, 47, 6, 1, 0, 'Avg', '***', 0, 'DP', 3548257], ['RB', 'Jimbo', 'Bishop', 31, 2, 10, 5, 8, 10, 12, 44, 3, 1, 0, 'Avg', '**', 0, 'DP', 8562484], ['CB', 'Samkelo', 'Bishop', 33, 3, 17, 16, 16, 7, 17, 68, 2, 3, 0, 'Avg', '***', 0, 'DP', 4235611], ['CB', 'Bo', 'Stimer', 24, 1, 14, 16, 5, 5, 17, 58, 3, 4, 0, 'Laid B', '*****', 0, 'DP', 3200970], ['LM', 'Simon', 'Ticker', 35, 1, 5, 5, 13, 16, 18, 57, 3, 1, 0, 'Avg', '*', 0, 'DP', 8290256], ['RM', 'Aj', 'Jean', 23, 3, 18, 16, 8, 6, 16, 60, 2, 4, 0, 'Avg', '*****', 0, 'DP', 2555510], ['RM', 'Bo', 'Hassan', 21, 1, 16, 12, 20, 7, 13, 59, 5, 4, 1, 'Leader', '***', 0, 'DP', 2835505], ['RM', 'Silver', 'Tubert', 25, 2, 17, 8, 10, 10, 13, 52, 1, 1, 0, 'Avg', '***', 0, 'DP', 2012292], ['RM', 'Ross', 'Stansfield', 31, 3, 14, 12, 9, 10, 7, 48, 1, 1, 1, 'Fighter', '***', 0, 'DP', 4849940], ['CM', 'Tommie', 'Reize', 19, 1, 16, 12, 18, 10, 12, 62, 3, 4, 1, 'Team P', '**', 0, 'DP', 8865693], ['CM', 'Banele', 'Bishop', 28, 3, 6, 12, 7, 11, 17, 48, 1, 1, 0, 'Avg', '***', 0, 'DP', 622877], ['CM', 'Banele', 'Tubert', 34, 3, 9, 8, 5, 5, 8, 30, 5, 2, 0, 'Avg', '*', 0, 'DP', 7566234], ['ST', 'Omar', 'Ribbenov', 19, 3, 17, 14, 16, 18, 7, 65, 5, 2, 0, 'Avg', '*****', 0, 'DP', 836487], ['ST', 'Banele', 'Del-Piero', 35, 1, 8, 20, 10, 20, 14, 61, 2, 2, 0, 'Avg', '*', 0, 'DP', 4112503], ['ST', 'Aj', 'Del-Piero', 27, 3, 17, 6, 17, 12, 5, 58, 4, 3, 0, 'Avg', '**', 0, 'DP', 3731537], ['ST', 'Barry', 'Smith', 35, 1, 18, 20, 8, 14, 16, 54, 2, 4, 0, 'Avg', '*', 0, 'DP', 5266273], ['ST', 'TJ', 'Smith', 20, 2, 20, 20, 11, 11, 6, 49, 5, 1, 0, 'Avg', '***', 0, 'DP', 7415303]]
 
-formation_4_3_3={'GK':1,"LB":1,"RB":1,"CB":2,"DM":0,"CM":3,"LM":0,"RM":0,"AM":0,"LW":0,"ST":3,"RW":0}
-formation_4_3_3_just_needed={k:v for (k,v) in formation_4_3_3.items() if v > 0}
-formation_4_3_3_list_keys=list(formation_4_3_3_just_needed.keys())
 
-avaliable_formations=[formation_4_4_2_just_needed,formation_4_3_3_just_needed]
-def formation_caculation():
-
-    #    D         A 
-    #GK =40%       3
-    #D=  40%       20
-    #DM= 8        4
-    #M= 20        20
-    #AM=4         8 
-    #S=0          50
-    #LM?
-    #RM?
-    #LW?
-    #RW?
-    #LB?
-    #RB?
-
-    #special:
-    #strong/weak core
-    #strong/weak flanks
-    #inexperience side
-    #side with leaders/team player/laid back
-    
-
-    pass
-
-def best_player(squad,position,num_player):
-    pass
-
-def find_player_position_in_team(list_to_check,playerid):
-
-    index_position=-1
-    for players_to_check in list_to_check:
-        index_position+=1
-        if players_to_check[11]==playerid:
-            return index_position
-    print ("ERROR - hmmm player not found based on playerid")
-    breakpoint()
-
-def index_and_delete_player(players_chosen_in,squad_temp_in):
-
-    #for chosen_player in players_chosen_in:
-    try:
-        index_position_of_player=find_player_position_in_team(list_to_check=squad_temp_in,playerid=players_chosen_in[0][11])
-        del squad_temp_in[index_position_of_player]
-        if args.verbose:
-            print ("info=", players_chosen_in)
-    except Exception as e:
-        print ("ERROR - oops something errored 222")
-        print ("ERROR=", e)
-        breakpoint()
-    return squad_temp_in
-
-            
-
-def best_team(formations_to_test=["formation_4_4_2_just_needed"]):
-    import random
-    global test_squad
-    squad=test_squad
-    team_built=[]
-    team_chosen=[]
-    #1b)
-    squad_temp=squad.copy()
-
-    # 1) loop through whats needed, 
-    # 1b) make a copy of the squad so i can delete players when added to our starting 11
-    # 2) loop through squad match what we have to step 1 i.e if i need 1 GK , create a shortlist of GK
-    # 3) order shortlist by skill
-    # 4) take number of players needed from shortlist and add to starting 11
-    # 5) delete player from squad_temp
-
-    #1)
-    for needs in avaliable_formations:
-    #for needs in [formation_4_4_2_just_needed.items(),formation_4_3_3_just_needed.items()]:
-        team_built=[]
-        team_chosen=[]
-        squad_temp=squad.copy()
-
-        if args.verbose:
-            print ("I am checking=",needs)
-        for bla in needs.items():
-            position_in_need=bla[0]
-            num_players_needed=bla[1]
-            
-            players_out_of_position_count=0
-            players_out_of_position={}
-            #formation_option="442"
-            if args.verbose:
-                print ("We need {} position={} ".format(num_players_needed, position_in_need))
-            players_of_intrest=[]
-            #2)
-            for player in squad_temp:
-                if position_in_need in player[0]:
-                    players_of_intrest.append(player)
-            #3)
-            get_n = itemgetter(4)
-            players_of_intrest_sorted=sorted(players_of_intrest, key=get_n,reverse=True)
-            if len(players_of_intrest_sorted) < num_players_needed:
-                #print ("branch 1")
-                if args.verbose:
-                    print ("only found {} {} and we need {}".format(len(players_of_intrest_sorted),position_in_need,num_players_needed))
-                    print("I will give you a player with the best stats")
-                #players_chosen=random.choice(players_of_intrest_sorted)
-                #print ("Player in Positon ...",players_chosen)
-                player_we_need=num_players_needed-len(players_of_intrest_sorted)
-                player_to_add=players_of_intrest_sorted[:num_players_needed]
-                team_chosen.append(player_to_add)
-                if args.verbose:
-                    print ("Player in Positon ...",player_to_add)
+def best_avliable_team():
+    #pop best players from the list
+    #what happens if a position is not avaliable?
+    # do i maybe need a copy of the list to edit rather than using Squad?
+    best_team_chosen=[]
+    players_found=0
+    #inelligent way to loop through all poistions , once found break the loop delete the players chosen and try again for the next posistion
+    while True:
+        #players_found=0
+        players_found=0
+        ids_to_delete=[]
+        #safety break
+        if len(best_team_chosen) > 11:
+            print("breaking for safety reasons")
+            break
+        #breakpoint()
+        for position,num_players_needed in formation_4_4_2_just_needed.items():
+            #players_found=0
+            #breakpoint()
+            #break out of this loop if we have all the players we need
+            print("loop details",position,num_players_needed)
+            if (num_players_needed == players_found) and players_found>0:
+                for delete_me_id in ids_to_delete:
+                    del squad[delete_me_id]
+                players_found=0
+                        
+            for player in squad:
+                player_index_position=0
+                #we have all the players we need
                 #breakpoint()
-                for random_player in range(0,player_we_need):
-                    players_out_of_position_count+=1
-                    players_out_of_position[position_in_need]=1
-                    if args.verbose:
-                        print("Best Avaliable player chosen for position...",players_chosen)
-                    try:
-                        player_chosen=random.choice(players_of_intrest_sorted)
-                    except Exception as e:
-                        print ("ERROR oops i errored ... =",e)
-                        breakpoint()
-                    #breakpoint()
-                    team_chosen.append(players_chosen)
-                    #i am using square brackets on player chosen to de complicate the index and delete process
-                    squad_temp=index_and_delete_player(players_chosen_in=[player_chosen],squad_temp_in=squad_temp)
+                if num_players_needed == players_found:
+                    print ("num of players needed found")
+                    break
+                else:
+                    if (position in player[0][0]) or (position in player[0]):
+                        print("i want to add a player",position,position in player)
+                        #breakpoint()
+                        best_team_chosen.append(player)
+                        ids_to_delete.append(player_index_position)
+                        print ("Player added to team chosed=",player)
+                        players_found+=1
+                        break
+                
+                player_index_position+=1
 
-            
-               # 31/10/22 issue is player is potentially being chosen twice as player delete is only happening at the end
-                #need to tweak logic to choose players i can and if any gaps then randomize
-            
-            elif len(players_of_intrest_sorted) ==0:
-                #print ("branch 2")
-                if args.verbose:
-                    print("{} position not found".format(position_in_need))
-                    print("I will give you a random player")
-                players_chosen=random.choice(players_of_intrest_sorted)
-                if args.verbose:
-                    print ("Player Chosen ...",players_chosen)
-                squad_temp=index_and_delete_player(players_chosen_in=player_chosen,squad_temp_in=squad_temp)
-                players_out_of_position_count+=1
-                players_out_of_position[position_in_need]=1
-                breakpoint()
-            else:
-                #print ("branch 3")
-                num_players_picked=0
-                players_chosen=players_of_intrest_sorted[:num_players_needed]
-                if args.verbose:
-                    print ("Player Chosen ...",players_chosen)
-                #4)
-                try:
-                    team_chosen.append(players_of_intrest_sorted[:num_players_needed])
-                    squad_temp=index_and_delete_player(players_chosen_in=players_chosen,squad_temp_in=squad_temp)
-                except:
-                    print ("ERROR Oops something went wrong 111")
-                    breakpoint()
-                #5)
-            
-        print("chosen team=")
-        for player3 in team_chosen:
-            print(player3)
-        #return(formation_option,players_out_of_position_count,players_out_of_position,team_chosen)
-        
-def balanced_team_score():
-    #aim for a 50 50 split across the team.
-    #pass through team and pick 5 best players over 23, 
-    #2nd pass through team and pick best players under 23 in positions still avaliable
-    #3rd pass fill in gaps
-    #if position not avaliable choose best player avaliable
-    
+    breakpoint()
+    create_team_score(first_11)
+    return()
+def best_young_team():
+    #look at best under 24 players and fill out the rest 
+    #scoring system? age+skill+younger players get an increased score + training speed
+    #then pop
+    def_score=55
+    att_score=55
+    return(def_score,att_score)
+def best_blended_team():
+    #need a scoring system like age+skill
+    #then pop
+    def_score=65
+    att_score=65
+    return(def_score,att_score)
+def create_team_score(first_11):
+    # average fitness
+    # def = GK*1.5 + DEF*4 + 50(MID) ?
+    # ata = ATA*50(MID)?
+    # lots of stuff
     pass
 
 
-def best_player_avaliable_score():
-    #pass through and get best avaliable player
-    #if position not avaliable choose best player avaliable
-    pass
+
+def formation_choice():
+    best_avliable_team()
+    best_young_team()
+    best_blended_team()
 
 
-def print_table():
-
-    print("         Best Avaliable  Best Young Team   Balanced team")
-    print("         D  A             D  A               D A")
-    print ("4-4-2   80 80            85 85              90 90  ")
-    print ("4-4-2 A 80 80            85 85              90 90  ")
-    print ("4-4-2 D 80 80            85 85              90 90  ")
-    print ("4-3-3   80 80            85 85              90 90  ")
+def offer_choice_to_user():
+    print ("Which formation do you want to choose")
+    print ("Average Team age, Average Overall team rating, gk rating, def overall, mid overall, ata overall,special overall")
+    print ("1) youth -28,70,65,45,80,21,5 ")
+    print ("2) best -55 ")
+    print ("3) balanced -55 ")
+    formation_methodology="Youth"
+    return (formation_methodology)
 
 
 
 def starting_preseason(squad):
-
-    print("...Pre-season...")
-    best_young_team_score()
-    balanced_team_score()
-    best_player_avaliable_score()
-    print_table()
-    import team_rating
-    season_formation=team_rating.team_formation()
-    type_of_team=team_rating.best_team()
-    print (season_formation)
+    formation_choice()
+    offer_choice_to_user()
+    #offer best avaliable v best young team v blend (scores)
+    #give user choice
+    pass
 
 if __name__=="__main__":
-    best_team()
+    global squad
+    squad=test_squad
+    starting_preseason(squad=test_squad)
